5014d4a1b6b6f57fc54b0e1655f52ed594c44029
prashant85 <dixitprashant855@gmail.com>
2013-01-12 16:51:45 +0530

    add envornment line

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ MadhulataInfotechHrm::Application.initialize!
 
 MadhulataInfotechHrm::Application.configure do
    config.action_mailer.delivery_method = :smtp
+   config.action_mailer.raise_delivery_errors = true
    config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com" ,
      :port => 587,
