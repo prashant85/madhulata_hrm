d370adab4dcf5d61a4e33e0ad7edad8208bb5f4d
prashant85 <dixitprashant855@gmail.com>
2013-01-15 18:39:34 +0530

    abc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,7 @@ MadhulataInfotechHrm::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'dry-brushlands-2732.herokuapp.com' }
-   config.action_mailer.raise_delivery_errors = true
-   config.consider_all_requests_local = true
+ 
 #   config.action_mailer.delivery_method = :smtp
 #   config.action_mailer.smtp_settings = {
 #     :enable_starttls_auto => true,
@@ -77,18 +75,21 @@ MadhulataInfotechHrm::Application.configure do
 #     :user_name => "dixitprashant855@gmail.com",
 #     :password => "9911551"
 # }
-  MadhulataInfotechHrm::Application.configure do
+   config.action_mailer.default_url_options = { :host => 'dry-brushlands-2732.herokuapp.com' }
+   config.action_mailer.raise_delivery_errors = true
+   
     config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
    config.action_mailer.default :charset => "utf-8"
-   config.action_mailer.smtp_settings = smtp_settings = {
-      :address              => "wp240.webpack.hosteurope.de",
-      :port                 => 25,
-      :domain               => "aviendu.com",
-      :user_name            => "wp10961995-avienduinfo",
-      :password             => "aviendu!222",
-      :authentication       => "plain",
-      :enable_starttls_auto => true
-  }
+   config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com" ,
+     :port => 587,
+     :domain => "gmail.com" ,
+     :authentication => "plain" ,
+     :user_name => "dixitprashant855@gmail.com" ,
+     :password => "9911551528" ,
+     :enable_starttls_auto => true
+    }
+    config.action_mailer.raise_delivery_errors = true
   end
 end
