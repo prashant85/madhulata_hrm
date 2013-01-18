6c1c167dcbd9ba56fffd4c6e5e7a44e04fee2df2
prashant85 <dixitprashant855@gmail.com>
2013-01-18 15:06:08 +0530

    abc

--- app/models/leaveapplication.rb
+++ app/models/leaveapplication.rb
@@ -3,24 +3,24 @@ class Leaveapplication < ActiveRecord::Base
    :user_id,:employee_detail_id, :commentonleave 
   belongs_to :user
   belongs_to :employee_detail
-  def casual_leave
-  	leave = Time.now.strftime("%m").to_i
-    if Time.now.strftime("%d").to_i < 14
-      leave = leave-1
-    end
-    leave
-  end
+  # def casual_leave
+  # 	leave = Time.now.strftime("%m").to_i
+  #   if Time.now.strftime("%d").to_i < 14
+  #     leave = leave-1
+  #   end
+  #   leave
+  # end
 
-  def joining
+  # def joining
 
-      EmployeeDetail.where(:id=> user_id)
-    end
+  #     EmployeeDetail.where(:id=> user_id)
+  #   end
 
-   def previlige_leave
-     self.joining
-   end
-   def total_day
-    Time.now.yday
+  #  def previlige_leave
+  #    self.joining
+  #  end
+  #  def total_day
+  #   Time.now.yday
      #dateofjoining=EmployeeDetail.find(@leaveapplication.employee_detail_id).join_date
     #   # EmployeeDetail.find(current_user.id).join_date
     #   if dateofjoining.strftime("%y").to_i != Time.now.strftime("%y").to_i
@@ -34,7 +34,7 @@ class Leaveapplication < ActiveRecord::Base
     #    end
     #     days =  dateofjoining.yday 
     #   # 364 - EmployeeDetail.find(current_user.id).join_date.yday + Time.now.yday 
-  end
+  # end
   
 
  
