018ab13a07cb772178c27464ab1f3b507fa1c2d1
prashant85 <dixitprashant855@gmail.com>
2013-01-18 15:46:00 +0530

    changes in production mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ MadhulataInfotechHrm::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
  
-config.action_mailer.default_url_options = { :host => 'rubyonrails4ror.com' }
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # config.after_initialize do
   #   ActiveMerchant::Billing::Base.mode = :test
