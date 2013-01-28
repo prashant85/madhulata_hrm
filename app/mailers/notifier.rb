a7402d21773536edd0290c33a08bbfa1845c0efe
prashant85 <dixitprashant855@gmail.com>
2013-01-28 13:57:14 +0530

    abc

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,7 +9,7 @@ class Notifier < ActionMailer::Base
   def sending_request(leaveapplication)
     @leaveapplication = leaveapplication 
      mail :to => "abhijeet@drupaltechie.com",
-          :bcc => ["renu@drupaltechie.com"],:cc => ["prashant@rubyonrails4ror.com"],
+          :bcc => ["renu@drupaltechie.com"],
          :subject => 'Leave application'
   end
 
